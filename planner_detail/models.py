944482a7dcf092088045acbe55013304c370c714
cny <chony5093@gmail.com>
2024-04-17 11:22:05 +0900

    add Planner model

--- planner_detail/models.py
+++ planner_detail/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 # Create your models here.
-
-class Cell(models.Model):   
-    cell_id = models.CharField(max_length=20)
-    cell_status = models.IntegerField(max_length=20)
-    place_id = models.CharField(max_length=30)
-    
         
+class Planner(models.Model):
+    user_id = models.CharField(max_length=20)
+    title = models.CharField(max_length=20)
+    cells = models.JSONField(blank=True,
+                             null=True)
+    planner_id = models.CharField(max_length=20)
+    dateTimeOfUpload = models.DateTimeField(auto_now=True)
+
+
 
