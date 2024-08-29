df0e5377c5ce3df087f37057665d090a19e8ab01
username <abc@abc.com>
2024-08-29 20:16:06 +0900

    수정

--- common/urls.py
+++ common/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
 
     path('register/', RegisterView.as_view()),
     path("login/", LoginView.as_view()),
-    path('delete/', delete_user_by_username, name='delete'),
+    path('delete/<str:username>/', delete_user_by_username, name='delete-user-by-username'),
     path('userinfo/<str:username>/', get_user_info, name='userinfo'),
     path('findusername/', UsernameRetrievalView.as_view(), name='findusername'),
     path('findpassword/', PasswordRetrievalView.as_view(), name='findpassword'),
